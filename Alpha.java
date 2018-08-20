87ed28362d385619f08a7ff781db4cce33f0554a
Praveena A M <praveenaarokiaraj@gmail.com>
2018-08-20 13:51:35 +0530

    Create Alpha.java

---
+++ Alpha.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class Alpha
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+char a;
+System.out.println("enter value");
+ a = sc.nextChar();
+if(a>='a' && a<='z')
+{
+System.out.println("alphabet");
+}
+else
+{
+System.out.println("not alphabet");
+}
+}
+}
